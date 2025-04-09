65a619f5d71ca563b7cefe3383c3fe85a7d21823
Canaisen <155198866+Canaisen@users.noreply.github.com>
2025-04-09 13:39:48 +0200

    Add files via upload

---
+++ Projects/CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+      static int factorial(int a){
+    int result = 1 ;
+
+    if (a>0)
+    {
+        for (int i = 1; i < a; i++)
+        {
+            result=result*i;
+        }
+        
+    }
+    return result;
+}
+
+       static void Main(string[] args){
+
+        Console.WriteLine(factorial(10));
+
+
+    }
+     
+    
+
+    
